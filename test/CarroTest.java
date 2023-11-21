72bf311fbd19a27dc1c804c04e3c2329850f53f6
Nicolas Baraldo <121053856+NicolasBaraldodeLima@users.noreply.github.com>
2023-11-21 17:48:24 -0300

    Add files via upload

---
+++ test/CarroTest.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Carro;
+
+public class CarroTest {
+
+        public static void main(String[] args) {
+            Carro carro = new Carro();
+            carro.nome = "Zoro";
+            carro.ano = 2020;
+            carro.modelo= "Honda";
+            System.out.println(carro.nome);
+            System.out.println(carro.modelo);
+            System.out.println(carro.ano);
+
+            System.out.println(carro.nome);
+            System.out.println(carro.modelo);
+            System.out.println(carro.ano);
+        }
+}
